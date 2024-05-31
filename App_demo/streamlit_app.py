5f40220d038cfd740171f80ebbeaf6afa8e688b5
Trgtuan10 <tuannguyentrong.402@gmail.com>
2024-05-31 16:39:41 +0700

    add controlnet, inpainting to demo

--- App_demo/streamlit_app.py
+++ App_demo/streamlit_app.py
@@ -5,6 +5,10 @@ from streamlit_image_select import image_select
 import sys
 sys.path.append('..')
 from base_gen import load_model_base, gen_interior_base
+from image_condition_gen import load_controlnet_model, gen_controlnet
+from inpainting_gen import load_model_inpaint, inpaint_gen
+from utils_func import translate_to_eng
+
 
 # UI configurations
 st.set_page_config(page_title="Interor and Architect Generator",
@@ -41,7 +45,7 @@ def configure_sidebar() -> None:
                                            value="the absolute worst quality, distorted features",
                                            help="This is a negative prompt, basically type what you don't want to see in the generated image",
                                            height=150)
-            imgae_controlnet = st.file_uploader("Upload img2img (optional)", type=["jpg", "jpeg", "png"], key="img2img_uploader")
+            image_controlnet = st.file_uploader("Upload img2img (optional)", type=["jpg", "jpeg", "png"], key="img2img_uploader")
             image_inpainting = st.file_uploader("Upload inpainting (optional) ", type=["jpg", "jpeg", "png"], key="inpainting_uploader")
             
             st.divider()
@@ -54,11 +58,11 @@ def configure_sidebar() -> None:
             submitted = st.form_submit_button(
                 "Submit", type="primary", use_container_width=True)
 
-        return option_gen, submitted, width, height, num_outputs, prompt, negative_prompt, imgae_controlnet, image_inpainting
+        return option_gen, submitted, width, height, num_outputs, prompt, negative_prompt, image_controlnet, image_inpainting
 
 
 def main_page(option_gen: str, submitted: bool, width: int, height: int, num_outputs: int,
-              prompt: str, negative_prompt: str, imgae_controlnet, image_inpainting) -> None:
+              prompt: str, negative_prompt: str, image_controlnet, image_inpainting) -> None:
     """Main page layout and logic for generating images.
 
     Args:
@@ -72,12 +76,15 @@ def main_page(option_gen: str, submitted: bool, width: int, height: int, num_out
     if submitted:
         #load 2 model types
         if option_gen == "Interior":
-            pipe, trigger_words = load_model_base()
+            model_path = "runwayml/stable-diffusion-v1-5"
             print("oke")
         else:
-            # pipe, trigger_words = load_controlnet_model()
+            model_path = "runwayml/stable-diffusion-v1-5"
             print("okewww")
-            
+        pipe = load_model_base(model_path)
+        pipe_controlnet= load_controlnet_model(model_path)
+        pipe_inpainting = load_model_inpaint(model_path)
+             
         with st.status('👩🏾‍🍳 Whipping up your words into art...', expanded=True) as status:
             st.write("⚙️ Model initiated")
             st.write("🙆‍♀️ Stand up and strecth in the meantime")
@@ -87,7 +94,32 @@ def main_page(option_gen: str, submitted: bool, width: int, height: int, num_out
                     # Calling the replicate API to get the image
                     with generated_images_placeholder.container():
                         all_images = []  # List to store all generated images
-                        output = gen_interior_base(pipe, prompt, trigger_words, negative_prompt, num_images=num_outputs, height=height, width=width)
+                        prompt = translate_to_eng(prompt)
+                        negative_prompt = translate_to_eng(negative_prompt)
+                        
+                        #generate image
+                        if image_controlnet is None and image_inpainting is None:
+                            output = gen_interior_base(pipe, 
+                                                       prompt, 
+                                                       trigger_words="", 
+                                                       neg=negative_prompt, 
+                                                       num_images=num_outputs, 
+                                                       eight=height, width=width)
+                        elif image_inpainting is None:
+                            output = gen_controlnet(pipe_controlnet, 
+                                                    prompt, trigger_words="", 
+                                                    neg=negative_prompt, 
+                                                    num_images=num_outputs, 
+                                                    height=height, width=width, 
+                                                    image=image_controlnet)
+                        else:
+                            output = inpaint_gen(pipe_inpainting, 
+                                                 image_controlnet, 
+                                                 image_inpainting, 
+                                                 prompt, negative_prompt, 
+                                                 num_images=num_outputs, 
+                                                 height=height, width=width)
+                        
                         if output:
                             st.toast(
                                 'Your image has been generated!', icon='😍')
@@ -112,25 +144,25 @@ def main_page(option_gen: str, submitted: bool, width: int, height: int, num_out
         pass
 
     # Gallery display for inspo
-    with gallery_placeholder.container():
-        img = image_select(
-            label="Like what you see? Right-click and save! It's not stealing if we're sharing! 😉",
-            images=[
-                "gallery/farmer_sunset.png", "gallery/astro_on_unicorn.png",
-                "gallery/friends.png", "gallery/wizard.png", "gallery/puppy.png",
-                "gallery/cheetah.png", "gallery/viking.png",
-            ],
-            captions=["A farmer tilling a farm with a tractor during sunset, cinematic, dramatic",
-                      "An astronaut riding a rainbow unicorn, cinematic, dramatic",
-                      "A group of friends laughing and dancing at a music festival, joyful atmosphere, 35mm film photography",
-                      "A wizard casting a spell, intense magical energy glowing from his hands, extremely detailed fantasy illustration",
-                      "A cute puppy playing in a field of flowers, shallow depth of field, Canon photography",
-                      "A cheetah mother nurses her cubs in the tall grass of the Serengeti. The early morning sun beams down through the grass. National Geographic photography by Frans Lanting",
-                      "A close-up portrait of a bearded viking warrior in a horned helmet. He stares intensely into the distance while holding a battle axe. Dramatic mood lighting, digital oil painting",
-                      ],
-            use_container_width=True
-        )
-    pass
+    # with gallery_placeholder.container():
+    #     img = image_select(
+    #         label="Like what you see? Right-click and save! It's not stealing if we're sharing! 😉",
+    #         images=[
+    #             "gallery/farmer_sunset.png", "gallery/astro_on_unicorn.png",
+    #             "gallery/friends.png", "gallery/wizard.png", "gallery/puppy.png",
+    #             "gallery/cheetah.png", "gallery/viking.png",
+    #         ],
+    #         captions=["A farmer tilling a farm with a tractor during sunset, cinematic, dramatic",
+    #                   "An astronaut riding a rainbow unicorn, cinematic, dramatic",
+    #                   "A group of friends laughing and dancing at a music festival, joyful atmosphere, 35mm film photography",
+    #                   "A wizard casting a spell, intense magical energy glowing from his hands, extremely detailed fantasy illustration",
+    #                   "A cute puppy playing in a field of flowers, shallow depth of field, Canon photography",
+    #                   "A cheetah mother nurses her cubs in the tall grass of the Serengeti. The early morning sun beams down through the grass. National Geographic photography by Frans Lanting",
+    #                   "A close-up portrait of a bearded viking warrior in a horned helmet. He stares intensely into the distance while holding a battle axe. Dramatic mood lighting, digital oil painting",
+    #                   ],
+    #         use_container_width=True
+    #     )
+    
 
 def main():
     """
